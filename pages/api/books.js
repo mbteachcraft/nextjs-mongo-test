19dc885511ea6bbc74ddc87185840e69194c163c
Maxi-SR <schmidtmaxi46@gmail.com>
2023-08-10 18:43:23 -0300

    cambio a books

--- pages/api/books.js
+++ pages/api/books.js
@@ -3,7 +3,7 @@ import clientPromise from "../../lib/mongodb";
 export default async (req, res) => {
    try {
        const client = await clientPromise;
-       const db = client.db("sample_mflix");
+       const db = client.db("testbooks");
 
        const movies = await db
            .collection("books")
